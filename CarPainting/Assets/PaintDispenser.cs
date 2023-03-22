730aef6ccc1dbfde894130dd583626205556d63f
Stefanstadd <98754444+Stefanstadd@users.noreply.github.com>
2023-03-22 13:42:16 +0100

    eWfaew
    feqwewqr

--- CarPainting/Assets/PaintDispenser.cs
+++ CarPainting/Assets/PaintDispenser.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.UI;
 using TMPro;
+
 public class PaintDispenser : MonoBehaviour
 {
     public Color outcome;
 
-    public Slider
+    public Slider slider1;
+    public TextMeshProUGUI slider1ValueText;
     // Start is called before the first frame update
     void Start()
     {
